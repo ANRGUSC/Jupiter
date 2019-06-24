26775c740b3ee0904e75d6d8bb378c3cc7ce5a59
caravansary83 <quynhngu@usc.edu>
2019-06-24 01:35:03 -0700

    users module

--- circe/original/evaluate.py
+++ circe/original/evaluate.py
@@ -15,6 +15,9 @@ import os
 import random
 import time
 import glob
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
 
 
 def evaluate_random():
@@ -132,35 +135,110 @@ def evaluate_combine_app(num_apps,num_samples):
     output_folder = 'output/'
     outfile = set()
     while 1:
-        time.sleep(120)
+        time.sleep(10)
+        print('Generate more random input files')
+        print('------')
         for (dirpath, dirnames, filenames) in os.walk(output_folder):
             for filename in filenames:
                 input_file = filename.split('_')[0] 
+                # print(input_file)
                 if input_file not in outfile:
-                    print(input_file)
+                    # print(input_file)
                     appname = input_file.split('-')[0]
-                    print(appname)
-                    print(input_file.split('-')[1])
+                    # print(appname)
+                    # print(input_file.split('-')[1])
                     curfile = input_file.split('-')[1].split('botnet')[0]
-                    print(curfile)
+                    # print(curfile)
                     curnum = int(curfile)+1
                     if curnum >num_samples: continue
-                    print('Generate more random input files')
+                    
                     newfile = 'sample_input/'+appname+'-'+str(curnum)+'botnet.ipsum'
                     print(newfile)
                     fileout ='input/'+appname+'-'+str(curnum)+'botnet.ipsum'
-                    print(fileout)
+                    # print(fileout)
                     shutil.copyfile(newfile,fileout)
                     outfile.add(input_file)
+        print('------')
         print(outfile)
 
 
+class MyHandler(PatternMatchingEventHandler):
+    """
+    Handling the event when there is a new file generated in ``OUTPUT`` folder
+    """
 
+    def process(self, event):
+        """
+        Log the time the file is created and calculate the execution time whenever there is an event.
+        
+        Args:
+            event: event to be watched for the ``OUTPUT`` folder
+        """
+
+        global start_times
+        global end_times
+        global exec_times
+        global count
+
+        """
+        event.event_type
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+        # the file will be processed there
+        if event.event_type == 'created':
+            # print('***************************************************')
+            print("Received file as output in evaluation - %s." % event.src_path)  
+            filename = os.path.split(event.src_path)[-1]
+            appname = filename.split('-')[0]
+            print(appname)
+            curfile = filename.split('-')[1].split('botnet')[0]
+            print(curfile)
+            curnum = int(curfile)+1
+            if curnum < num_samples: 
+                newfile = 'sample_input/'+appname+'-'+str(curnum)+'botnet.ipsum'
+                print(newfile)
+                fileout ='input/'+appname+'-'+str(curnum)+'botnet.ipsum'
+                # print(fileout)
+                shutil.copyfile(newfile,fileout)
+
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
+
+        
+        
 
 if __name__ == '__main__':
     #evaluate_random()
     time.sleep(60)
     print('Start copying sample files for evaluation')
-    evaluate_sequential()
+    # evaluate_sequential()
     #evaluate_test()
-    # evaluate_combine_app(100,10)
+    #evaluate_combine_app(100,10)
+    global num_apps, num_samples
+    num_apps = 100
+    num_samples = 10
+    print('---- Generate random input files')
+    for i in range(1,num_apps+1):
+        filein = 'sample_input/dummyapp%d-1botnet.ipsum'%(i)
+        fileout ='input/dummyapp%d-1botnet.ipsum'%(i)
+        shutil.copyfile(filein,fileout) 
+    
+    print("Starting the output monitoring system:")
+    observer = Observer()
+    observer.schedule(MyHandler(), path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'output/'))
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
