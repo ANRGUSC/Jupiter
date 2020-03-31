111d03a50e42d183ca1270e02416ea85c4ac98ff
Jason A. Tran <jasonatran@gmail.com>
2020-03-30 21:42:02 -0700

    format styling and delete commented code

--- mulhome_scripts/delete_all_waves.py
+++ mulhome_scripts/delete_all_waves.py
@@ -15,18 +15,19 @@ import utilities
 import time
 import logging
 
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
-def write_file(filename,message):
-    with open(filename,'a') as f:
+
+def write_file(filename, message):
+    with open(filename, 'a') as f:
         f.write(message)
 
-def delete_all_waves(app_name):
-    """Tear down all WAVE deployments.
-    """
-    
 
-    
+def delete_all_waves(app_name):
+    """
+        Tear down all WAVE deployments.
+    """
+
     jupiter_config.set_globals()
 
     """
@@ -39,23 +40,25 @@ def delete_all_waves(app_name):
     dag = dag_info[1]
     logging.debug('Starting to teardown WAVE')
     if jupiter_config.BOKEH == 3:
-        latency_file = '../stats/exp8_data/summary_latency/system_latency_N%d_M%d.log'%(len(nodes),len(dag))
+        latency_file = '../stats/exp8_data/summary_latency/system_latency_N%d_M%d.log' % (len(nodes), len(dag))
         start_time = time.time()
-        msg = 'WAVE teardownstart %f \n'%(start_time)
-        write_file(latency_file,msg)
+        msg = 'WAVE teardownstart %f \n' % start_time
+        write_file(latency_file, msg)
 
     """
         This loads the kubernetes instance configuration.
         In our case this is stored in admin.conf.
         You should set the config file path in the jupiter_config.py file.
     """
-    config.load_kube_config(config_file = jupiter_config.KUBECONFIG_PATH)
+    config.load_kube_config(config_file=jupiter_config.KUBECONFIG_PATH)
 
     """
-        Loop through the list of nodes and deletes the all profiler related k8 deployment, replicaset, pods, and service.
-        The deletion should follow this particular order for a proper removal.
-        You can always check if a service/pod/deployment is running after running this script via kubectl command.
-        E.g., 
+        Loop through the list of nodes and deletes the all profiler related k8
+        deployment, replicaset, pods, and service. The deletion should follow
+        this particular order for a proper removal. You can always check if a
+        service/pod/deployment is running after running this script via kubectl
+        command.
+        E.g.,
             kubectl get svc -n "namespace name"
             kubectl get deployement -n "namespace name"
             kubectl get replicaset -n "namespace name"
@@ -67,55 +70,52 @@ def delete_all_waves(app_name):
         # We have defined the namespace for deployments in jupiter_config
         namespace = jupiter_config.MAPPER_NAMESPACE
 
-        # Get proper handles or pointers to the k8-python tool to call different functions.
+        # Get proper handles or pointers to the k8-python tool to call
+        # different functions.
         k8s_apps_v1 = client.AppsV1Api()
         body = client.V1DeleteOptions()
-        
+
         # First check if there is a exisitng profiler deployment with
         # the name = key in the respective namespace
 
-        pod_name = app_name+'-'+key 
-        #pod_name = key
+        pod_name = app_name + '-' + key
         resp = None
         try:
             resp = k8s_apps_v1.read_namespaced_deployment(pod_name, namespace)
         except ApiException as e:
             logging.debug("Exception Occurred")
 
-        # if a deployment with the name = key exists in the namespace, delete it
+        # if a deployment with the name = key exists in namespace, delete it
         if resp:
-            # del_resp_0 = k8s_apps_v1.delete_namespaced_deployment(pod_name, namespace, body)
-            del_resp_0 = k8s_apps_v1.delete_namespaced_deployment(pod_name, namespace)
+            del_resp_0 = k8s_apps_v1.delete_namespaced_deployment(pod_name,
+                                                                  namespace)
 
-            logging.debug("Deployment '%s' Deleted. status='%s'" % (pod_name, str(del_resp_0.status)))
+            logging.debug("Deployment '%s' Deleted. status='%s'" %
+                          (pod_name, str(del_resp_0.status)))
 
+        # Check if there is a replicaset running by using the label "app=wave_"
+        # + key e.g, "app=wave_node1". The label of kubernets are used to
+        # identify replicaset associate to each task
 
-        # Check if there is a replicaset running by using the label "app=wave_" + key e.g, "app=wave_node1"
-        # The label of kubernets are used to identify replicaset associate to each task
-        
-        label = "app="+app_name+'-wave_'+key
-        #label = key
-        resp = k8s_apps_v1.list_namespaced_replica_set(label_selector = label,namespace=namespace)
+        label = "app=" + app_name + '-wave_' + key
+        resp = k8s_apps_v1.list_namespaced_replica_set(label_selector= label,
+                                                       namespace=namespace)
         # if a replicaset exist, delete it
-        # pprint(resp)
-        # logging.debug resp.items[0].metadata.namespace
         for i in resp.items:
             if i.metadata.namespace == namespace:
                 try:
-                    # del_resp_1 = k8s_apps_v1.delete_namespaced_replica_set(i.metadata.name, namespace, body)
                     del_resp_1 = k8s_apps_v1.delete_namespaced_replica_set(i.metadata.name, namespace)
-
-                    logging.debug("Relicaset '%s' Deleted. status='%s'" % (pod_name, str(del_resp_1.status)))
+                    logging.debug("Relicaset '%s' Deleted. status='%s'" %
+                                  (pod_name, str(del_resp_1.status)))
                 except ApiException as e:
-                    logging.debug("Exception when calling AppsV1Api->delete_namespaced_replica_set: %s",e)
+                    logging.debug("Exception when calling AppsV1Api->delete_namespaced_replica_set: %s", e)
 
         # Check if there is a pod still running by using the label
         resp = None
         api_2 = client.CoreV1Api()
-        resp = api_2.list_namespaced_pod(namespace, label_selector = label)
+        resp = api_2.list_namespaced_pod(namespace, label_selector=label)
         # if a pod is running just delete it
         if resp.items:
-            # del_resp_2 = api_2.delete_namespaced_pod(resp.items[0].metadata.name, namespace, body)
             del_resp_2 = api_2.delete_namespaced_pod(resp.items[0].metadata.name, namespace)
 
             logging.debug("Pod Deleted. status='%s'" % str(del_resp_2.status))
@@ -130,20 +130,22 @@ def delete_all_waves(app_name):
         # if a service is running, kill it
         if resp:
             del_resp_2 = api_2.delete_namespaced_service(pod_name, namespace)
-            # del_resp_2 = api_2.delete_namespaced_service(pod_name, namespace,body)
-            logging.debug("Service Deleted. status='%s'" % str(del_resp_2.status))
+            logging.debug("Service Deleted. status='%s'" %
+                          str(del_resp_2.status))
 
-        # At this point you should not have any of the profiler related service, pod, or deployment running     
+    # At this point you should not have any of the profiler related service, 
+    # pod, or deployment running
     logging.debug('Successfully teardown WAVE ')
     if jupiter_config.BOKEH == 3:
         end_time = time.time()
-        msg = 'WAVE teardownend %f \n'%(end_time)
-        write_file(latency_file,msg)
+        msg = 'WAVE teardownend %f \n' % (end_time)
+        write_file(latency_file, msg)
         teardown_time = end_time - start_time
-        logging.debug('Time to teardown WAVE'+ str(teardown_time))
+        logging.debug('Time to teardown WAVE' + str(teardown_time))
+
 
 if __name__ == '__main__':
     jupiter_config.set_globals() 
     app_name = jupiter_config.APP_OPTION
-    app_name = app_name+'1'
+    app_name = app_name + '1'
     delete_all_waves(app_name)
