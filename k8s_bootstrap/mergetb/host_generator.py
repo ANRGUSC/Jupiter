c5dd54a5ecba1173ff2f888e08f0769045bae32b
Jason A. Tran <jasonatran@gmail.com>
2020-03-30 20:54:41 -0700

    update mergetb instructions, change ansible to idempotent plays, restructure bootstrap scripts for both ubuntu/debian

--- k8s_bootstrap/mergetb/host_generator.py
+++ k8s_bootstrap/mergetb/host_generator.py
@@ -19,12 +19,12 @@ model_file = sys.argv[1]
 import_name = model_file.split(".")[0]
 model = importlib.import_module(import_name)
 
-with open("ansible/hosts", 'w') as g:
+with open("hosts", 'w') as g:
     g.write("[master]\n")
     for m in model.JUPITER_MASTER_NODES:
         g.write("{}\n".format(m))
 
-    g.write("\n[nodes]\n")
+    g.write("\n[workers]\n")
     for w in model.JUPITER_WORKER_NODES:
         g.write("{}\n".format(w))
 
