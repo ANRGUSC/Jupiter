da96b451c1aec5c2fea1eb407f30f4303e2899d7
Jason A. Tran <jasonatran@gmail.com>
2020-03-30 21:33:58 -0700

    change print statement and format styling

--- mulhome_scripts/build_push_wave.py
+++ mulhome_scripts/build_push_wave.py
@@ -10,39 +10,41 @@ import configparser
 import jupiter_config
 import logging
 
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def build_push_wave():
-    """Build WAVE home and worker image from Docker files and push them to the Dockerhub.
+    """Build WAVE home and worker image from Docker files and push them to the
+       Dockerhub.
     """
     jupiter_config.set_globals()
-    INI_PATH  = jupiter_config.APP_PATH + 'app_config.ini'
+    INI_PATH = jupiter_config.APP_PATH + 'app_config.ini'
     config = configparser.ConfigParser()
     config.read(INI_PATH)
     sys.path.append(jupiter_config.WAVE_PATH)
     logging.debug(jupiter_config.WAVE_PATH)
 
-    import wave_docker_files_generator as dc 
+    import wave_docker_files_generator as dc
     os.chdir(jupiter_config.WAVE_PATH)
 
     logging.debug(jupiter_config.APP_NAME)
 
-    home_file = dc.write_wave_home_docker(app_file = jupiter_config.APP_NAME, ports = jupiter_config.FLASK_DOCKER)
-    worker_file = dc.write_wave_worker_docker(app_file = jupiter_config.APP_NAME, ports = jupiter_config.FLASK_DOCKER)
+    home_file = dc.write_wave_home_docker(app_file=jupiter_config.APP_NAME,
+                                          ports=jupiter_config.FLASK_DOCKER)
+    worker_file = dc.write_wave_worker_docker(app_file=jupiter_config.APP_NAME,
+                                              ports=jupiter_config.FLASK_DOCKER)
 
-
-    print(home_file)
-    print(worker_file)
-    cmd = "sudo docker build -f %s ../../../ -t %s"%(home_file,jupiter_config.WAVE_HOME_IMAGE)
+    logging.debug(home_file)
+    logging.debug(worker_file)
+    cmd = "sudo docker build -f %s ../../../ -t %s" % \
+        (home_file, jupiter_config.WAVE_HOME_IMAGE)
     os.system(cmd)
     os.system("sudo docker push " + jupiter_config.WAVE_HOME_IMAGE)
-    
-    cmd = "sudo docker build -f %s ../../../ -t %s"%(worker_file,jupiter_config.WAVE_WORKER_IMAGE)
+
+    cmd = "sudo docker build -f %s ../../../ -t %s" % \
+        (worker_file, jupiter_config.WAVE_WORKER_IMAGE)
     os.system(cmd)
     os.system("sudo docker push " + jupiter_config.WAVE_WORKER_IMAGE)
-
-
 
 if __name__ == '__main__':
     build_push_wave()
