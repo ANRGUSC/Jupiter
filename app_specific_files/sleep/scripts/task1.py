b8c4e5d9c63086ae04c287bad9a2ef75fcc489e9
Wasd5 <zhao115@usc.edu>
2020-04-01 15:23:47 -0700

    sleep app

--- app_specific_files/sleep/scripts/task1.py
+++ app_specific_files/sleep/scripts/task1.py
@@ -10,9 +10,10 @@ def task(input_files, pathin, pathout):
 
     # input_files is a string, not a lost
     time.sleep(50)
-    cmd = "dd bs=1024 count=8192 </dev/urandom >%s/%s_%s" % (pathout, input_files, sys.argv[0].split('.')[0])
+    output_files = input_files + '_' + sys.argv[0].split('.')[0]
+    cmd = "dd bs=1024 count=8192 </dev/urandom >%s/%s" % (pathout, output_files)
     os.system(cmd)
-    return [os.path.join(pathout,input_files + sys.argv[0])]
+    return [os.path.join(pathout, output_files)]
 
 
 
@@ -21,12 +22,11 @@ def main():
     outpath = os.path.join(os.path.dirname(__file__), "generated_files/")
     outfile = task(filelist, outpath, outpath)
     return outfile
-    
-'''
+
 if __name__ == '__main__':
 
     #Suppose the file structure is erick/detection_app/camera1_input/camera1_20190222.jpeg
-    filelist= 'input_0'
-    task(filelist, '/home/erick/detection_app/camera1_input', '/home/erick/detection_app')
-'''
+    filelist= 'input0'
+    task(filelist, '../sample_input', '.')
+
 
