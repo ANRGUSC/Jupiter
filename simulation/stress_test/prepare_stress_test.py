5a927a75a3a46da2eabd53327ee60fc57bd03faf
Quynh <quynhngu@usc.edu>
2020-04-04 20:14:19 -0700

    prepare stress test on XDC 2

--- simulation/stress_test/prepare_stress_test.py
+++ simulation/stress_test/prepare_stress_test.py
@@ -13,7 +13,7 @@ logging.basicConfig(level = logging.DEBUG)
 
 # This script must run on XDC to put random stress test on random nodes in order to collect stats from DCOMP testbed 
 
-def get_worker_nodes(node_info_file):
+def get_nodes(node_info_file):
   """read the node info from the file input
   
   Args:
@@ -23,36 +23,42 @@ def get_worker_nodes(node_info_file):
       dict: node information 
   """
   nodes = []
+  homes = []
   node_file = open(node_info_file, "r")
   for line in node_file:
       node_line = line.strip().split(" ")
-      if node_line[0].startswith('home'): continue
-      nodes.append(node_line[1])
-  return nodes
+      if node_line[0].startswith('home'): 
+        homes.append(node_line[1])
+      else:
+        nodes.append(node_line[1])
+  return homes, nodes
 
-def build_push_stress():
+def build_push_stress(homes):
     jupiter_config.set_globals()
-    stress_file = 'Dockerfile'
-    cmd = "sudo docker build -f %s . -t %s"%(stress_file,jupiter_config.STRESS_IMAGE)
-    os.system(cmd)
-    os.system("sudo docker push " + jupiter_config.STRESS_IMAGE) 
+    ssh = connect_remote_ssh(hostname)
+    cmd_to_execute = "sudo docker build -f Dockerfile . -t %s"%(jupiter_config.STRESS_IMAGE)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
+    cmd_to_execute = "sudo docker push " + jupiter_config.STRESS_IMAGE
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
 
-def gen_random_stress():
+def gen_random_stress(nodes):
     jupiter_config.set_globals()
     num_stress = jupiter_config.NUM_STRESS
-    path1 = '../../nodes.txt'
-    nodes = get_worker_nodes(path1)
     random_stressed_nodes = random.choices(nodes, k=num_stress)
     return random_stressed_nodes
+
+def connect_remote_ssh(hostname)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # no known_hosts error
+    myuser = os.getlogin()
+    mySSHK = '/home/%s/.ssh/id_rsa.pub'%(myuser)
+    ssh.connect(hostname, username=myuser, key_filename=mySSHK)
+    return ssh
 
 def run_remote(random_stressed_nodes):
     jupiter_config.set_globals()
     for hostname in random_stressed_nodes:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # no known_hosts error
-        myuser = os.getlogin()
-        mySSHK = '/home/%s/.ssh/id_rsa.pub'%(myuser)
-        ssh.connect(hostname, username=myuser, key_filename=mySSHK)
+        ssh = connect_remote_ssh(hostname)
         cmd_to_execute = "sudo docker pull "+ jupiter_config.STRESS_IMAGE
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
         # cmd = "sudo docker run -d --name sim "+ jupiter_config.STRESS_IMAGE
@@ -61,9 +67,11 @@ def run_remote(random_stressed_nodes):
         # os.system(cmd)
 
 def prepare_stress_test():
-    build_push_stress()
-    random_stressed_nodes = gen_random_stress()
-    run_remote(random_stressed_nodes)
+    node_info_file = '../../nodes.txt'
+    homes, nodes = get_nodes(node_info_file)
+    build_push_stress(homes)
+    # random_stressed_nodes = gen_random_stress()
+    # run_remote(random_stressed_nodes)
     
 if __name__ == '__main__':
     prepare_stress_test()
