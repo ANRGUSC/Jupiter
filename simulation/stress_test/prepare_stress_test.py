2c63b21d4c1c0b489397a52eb50dc9ece309ca2c
Quynh <quynhngu@usc.edu>
2020-04-04 23:14:01 -0700

    run multiple tests with debug info and flask

--- simulation/stress_test/prepare_stress_test.py
+++ simulation/stress_test/prepare_stress_test.py
@@ -7,9 +7,14 @@ import logging
 import random
 import paramiko
 import socket
+import _thread
+import multiprocessing
+from multiprocessing import Process, Manager
 
 
-logging.basicConfig(level = logging.INFO)
+
+logging.basicConfig(level = logging.DEBUG)
+app = Flask(__name__)
 
 # This script must run on XDC to put random stress test on random nodes in order to collect stats from DCOMP testbed 
 
@@ -41,11 +46,11 @@ def build_push_stress():
     cmd_to_execute = '(cd Jupiter/simulation/stress_test/; sudo docker build -f Dockerfile . -t %s)'%(jupiter_config.STRESS_IMAGE)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
     for line in ssh_stdout:
-        print(line)
+        logging.debug(line)
     cmd_to_execute = "sudo docker push " + jupiter_config.STRESS_IMAGE
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
     for line in ssh_stdout:
-        print(line)
+        logging.debug(line)
 
 def gen_random_stress(nodes):
     jupiter_config.set_globals()
@@ -61,22 +66,29 @@ def connect_remote_ssh(hostname):
     ssh.connect(hostname, username=myuser, key_filename=mySSHK)
     return ssh
 
-def run_remote(random_stressed_nodes):
+def run_remote_stress(hostname):
+    print('-----------------')
     jupiter_config.set_globals()
+    print('-----------------2')
+    ssh = connect_remote_ssh(hostname)
+    print('-----------------3')
+    cmd_to_execute = "sudo docker pull "+ jupiter_config.STRESS_IMAGE
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
+    for line in ssh_stdout:
+        logging.debug(line)
+    cmd_to_execute = "sudo docker run -d --name sim "+ jupiter_config.STRESS_IMAGE
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
+    for line in ssh_stdout:
+        logging.debug(line)
+    cmd_to_execute = "sudo docker exec -it sim python3 /stress_test.py"
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
+    for line in ssh_stdout:
+        logging.debug(line)
+
+def run_remote(random_stressed_nodes):
     for hostname in random_stressed_nodes:
-        ssh = connect_remote_ssh(hostname)
-        cmd_to_execute = "sudo docker pull "+ jupiter_config.STRESS_IMAGE
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
-        for line in ssh_stdout:
-            print(line)
-        cmd_to_execute = "sudo docker run -d --name sim "+ jupiter_config.STRESS_IMAGE
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
-        for line in ssh_stdout:
-            print(line)
-        cmd_to_execute = "sudo docker exec -it sim python3 /stress_test.py"
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
-        for line in ssh_stdout:
-            print(line)
+        print('-----------------5')
+        _thread.start_new_thread(run_remote_stress,(hostname,))    
 
 def prepare_stress_test():
     node_info_file = '../../nodes.txt'
@@ -84,6 +96,19 @@ def prepare_stress_test():
     build_push_stress()
     random_stressed_nodes = gen_random_stress(nodes)
     run_remote(random_stressed_nodes)
+
+class MonitorRecv(multiprocessing.Process):
+    def __init__(self):
+        multiprocessing.Process.__init__(self)
+
+    def run(self):
+        """
+        Start Flask server
+        """
+        logging.debug("Flask server started")
+        app.run(host='0.0.0.0')
     
 if __name__ == '__main__':
+    web_server = MonitorRecv()
+    web_server.start()
     prepare_stress_test()
