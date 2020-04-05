6ffc74b9af2243d63def3b44262e3006fb37fa29
Quynh <quynhngu@usc.edu>
2020-04-04 22:49:41 -0700

    debug paramiko 2

--- simulation/stress_test/prepare_stress_test.py
+++ simulation/stress_test/prepare_stress_test.py
@@ -37,7 +37,8 @@ def build_push_stress(homes):
     jupiter_config.set_globals()
     print(jupiter_config.STRESS_IMAGE)
     ssh = connect_remote_ssh(homes[0])
-    cmd_to_execute = "sudo docker build -f Dockerfile . -t %s"%(jupiter_config.STRESS_IMAGE)
+    # cmd_to_execute = '(cd Jupiter/simulation/stress_test/; sudo docker build -f Dockerfile . -t %s)'%(jupiter_config.STRESS_IMAGE)
+    cmd_to_execute ='cd ..; pwd'
     print(cmd_to_execute)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute, get_pty=True)
     # ssh_stdout.channel.recv_exit_status()
