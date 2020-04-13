d87762f4d8911266b0ebca7aa0c26164b3f278ae
Wasd5 <zhao115@usc.edu>
2020-04-13 12:14:18 -0700

    throughput HEFT

--- task_mapper/heft_mulhome/heft_throughput/heft_dup.py
+++ task_mapper/heft_mulhome/heft_throughput/heft_dup.py
@@ -45,6 +45,7 @@ class Task:
         self.comp_cost = []
         self.avg_comp = 0
         self.pre_task_num = -1
+        self.weight = 0
 
 class Processor:
 
@@ -55,6 +56,13 @@ class Processor:
         self.number = num
         self.time_line = []
 
+class Link:
+    """Link class represent a VIRTUAL link
+    consider both link and nodes as computation resources
+    here we use virtual links, meaning imagine we have an independent link
+    between each pair of nodes, and we try to balance the calculation time
+    among them and the computing nodes
+    """
  
 class HEFT:
     """A class of scheduling algorithm
@@ -89,11 +97,79 @@ class HEFT:
 
         for task in self.tasks:
             task.avg_comp = sum(task.comp_cost) / self.num_processor
-
+        
+        """ throughput optimized HEFT: try to balance the execution time among all nodes """
+        
+        '''
+        step1: rank tasks in decreasing order by their 'weight'
+               weight of a task is defined as the average computation time, plus the max of (parent weight + ave communication time)
+        '''
+        for task in self.tasks:
+            self.cal_weight(task)
+        self.tasks.sort(cmp=lambda x, y: cmp(x.weight, y.weight), reverse=True)
+        
+        '''
+        step2: for task in decreasing order of weight, assign each task to a processor to minimize the max occupation time of
+               all resources, including links and processors
+        '''
+        for task in self.tasks:
+            
+        
         self.cal_up_rank(self.tasks[self.start_task_num])
         self.cal_down_rank(self.tasks[self.end_task_num])
-        self.tasks.sort(cmp=lambda x, y: cmp(x.up_rank, y.up_rank), reverse=True)
 
+    def cal_up_rank(self, task):
+        """
+        Calculate the upper rank of all tasks.
+        
+        Args:
+            task (str): the entry node of the DAG
+        """
+        longest = 0
+        for successor in self.tasks:
+            if self.data[task.number][successor.number] != -1:
+                if successor.up_rank == -1:
+                    self.cal_up_rank(successor)
+
+                longest = max(longest, self.cal_avg_comm(task, successor) + successor.up_rank)
+
+        task.up_rank = task.avg_comp + longest
+        
+    def cal_down_rank(self, task):
+        """
+        Calculate the down rank of all tasks.
+
+        Args:
+            task (str): the exit node of the DAG.
+        """
+        if task == self.tasks[self.start_task_num]:
+            task.down_rank = 0
+            return
+        for pre in self.tasks:
+            if self.data[pre.number][task.number] != -1:
+                if pre.down_rank == -1:
+                    self.cal_down_rank(pre)
+
+                task.down_rank = max(task.down_rank,
+                                     pre.down_rank + pre.avg_comp + self.cal_avg_comm(pre, task))
+                                     
+    def cal_weight(self, task):
+    """
+    """
+        if task.weight != 0:
+            return 
+        if task == self.tasks[self.start_task_num]:
+            task.weight = task.ave_comp
+            return
+        max_parent_cost = 0
+        for parent in self.tasks:
+            if self.data[parent.number][task.number] != -1:
+                if parent.weight == 0:
+                    cal_weight(parent)
+                max_parent_cost = max(max_parent_cost, parent.weight + cal_avg_comm(parent, task)) 
+        task.weight = max_parent_cost + task.ave_comp
+        
+        
     def cal_comm_quadratic(self,file_size,quaratic_profile):
         """communication quadratic information
         
@@ -105,6 +181,7 @@ class HEFT:
             float: predicted transfer time
         """
         return (np.square(file_size)*quaratic_profile[0] + file_size*quaratic_profile[1] + quaratic_profile[2])
+
 
     def cal_avg_comm(self, task1, task2):
         """
@@ -129,169 +206,8 @@ class HEFT:
             exit()
         return res / (self.num_processor ** 2 - self.num_processor)
 
-    def cal_up_rank(self, task):
-        """
-        Calculate the upper rank of all tasks.
-        
-        Args:
-            task (str): the entry node of the DAG
-        """
-        longest = 0
-        for successor in self.tasks:
-            if self.data[task.number][successor.number] != -1:
-                if successor.up_rank == -1:
-                    self.cal_up_rank(successor)
-
-                longest = max(longest, self.cal_avg_comm(task, successor) + successor.up_rank)
-
-        task.up_rank = task.avg_comp + longest
-
-    def cal_down_rank(self, task):
-        """
-        Calculate the down rank of all tasks.
-
-        Args:
-            task (str): the exit node of the DAG.
-        """
-        if task == self.tasks[self.start_task_num]:
-            task.down_rank = 0
-            return
-        for pre in self.tasks:
-            if self.data[pre.number][task.number] != -1:
-                if pre.down_rank == -1:
-                    self.cal_down_rank(pre)
-
-                task.down_rank = max(task.down_rank,
-                                     pre.down_rank + pre.avg_comp + self.cal_avg_comm(pre, task))
-
-    # Modified communication part
-    def cal_est(self, task, processor):
-        """
-        Calculate the earliest start time of task on processor.
-        
-        Args:
-            - task (str): the task name
-            - processor (str): the processor name
-        
-        Returns:
-            TYPE: estimated execution time of the task on the processor
-        """
-        est = 0
-        for pre in self.tasks:
-            if self.data[pre.number][task.number] != -1:
-                if pre.processor_num != processor.number:
-                    for dup_task in self.dup_tasks:
-                        if dup_task.number == pre.number and dup_task.processor_num == task.processor_num:
-                            c = 0
-                            break
-                    else:
-                        c = self.cal_comm_quadratic(self.data[pre.number][task.number],self.quaratic_profile[pre.processor_num][processor.number])
-                else:
-                    c = 0
-                if pre.aft + c > est:
-                    est = pre.aft + c
-                    self.critical_pre_task_num = pre.number
-
-                #est = max(est, pre.aft + c)
-
-        time_slots = []
-        if len(processor.time_line) == 0:
-            time_slots.append([0, 9999])
-        else:
-            for i in range(len(processor.time_line)):
-                if i == 0:
-                    if processor.time_line[i].start != 0:
-                        time_slots.append([0, processor.time_line[i].start])
-                    else:
-                        continue
-                else:
-                    time_slots.append([processor.time_line[i - 1].end, processor.time_line[i].start])
-            time_slots.append([processor.time_line[len(processor.time_line) - 1].end, 9999])
-
-
-        for slot in time_slots:
-            if est < slot[0] and slot[0] + task.comp_cost[processor.number] <= slot[1]:
-                return slot[0]
-            if est >= slot[0] and est + task.comp_cost[processor.number] <= slot[1]:
-                return est
-        # TODO: Possible bug here. If the value of est is larger than 9999 it returns an empty array which creates failts.
-        # So added a default return statement to always return something. Not sure whether it is correct
-        return est
-
-    # Original
-    # def cal_est(self, task, processor):
-    #     """
-    #     Calculate the earliest start time of task on processor.
-    #     """
-    #     est = 0
-    #     for pre in self.tasks:
-    #         if self.data[pre.number][task.number] != -1:
-    #             if pre.processor_num != processor.number:
-    #                 for dup_task in self.dup_tasks:
-    #                     if dup_task.number == pre.number and dup_task.processor_num == task.processor_num:
-    #                         c = 0
-    #                         break
-    #                 else:
-    #                     c = self.data[pre.number][task.number] / self.rate[pre.processor_num][processor.number]
-    #             else:
-    #                 c = 0
-    #             if pre.aft + c > est:
-    #                 est = pre.aft + c
-    #                 self.critical_pre_task_num = pre.number
-    #
-    #             #est = max(est, pre.aft + c)
-    #
-    #     time_slots = []
-    #     if len(processor.time_line) == 0:
-    #         time_slots.append([0, 9999])
-    #     else:
-    #         for i in range(len(processor.time_line)):
-    #             if i == 0:
-    #                 if processor.time_line[i].start != 0:
-    #                     time_slots.append([0, processor.time_line[i].start])
-    #                 else:
-    #                     continue
-    #             else:
-    #                 time_slots.append([processor.time_line[i - 1].end, processor.time_line[i].start])
-    #         time_slots.append([processor.time_line[len(processor.time_line) - 1].end, 9999])
-    #
-    #     for slot in time_slots:
-    #         if est < slot[0] and slot[0] + task.comp_cost[processor.number] <= slot[1]:
-    #             return slot[0]
-    #         if est >= slot[0] and est + task.comp_cost[processor.number] <= slot[1]:
-    #             return est
-
-    def duplicate(self):
-        """
-        Reduce the communication overhead according copying the redundant tasks.
-        """
-        for pre_task in self.tasks:
-            pre_processor_num = pre_task.processor_num
-            for task in self.tasks:
-                if pre_task == task or task.pre_task_num != pre_task.number:
-                    continue
-                processor_num = task.processor_num
-                if pre_processor_num == processor_num:
-                    continue
-                dup_task_ast = self.cal_est(pre_task, self.processors[processor_num])
-                dup_task_aft = dup_task_ast + pre_task.comp_cost[processor_num]
-                if dup_task_aft > task.ast:
-                    continue
-
-                dup_task = Task(pre_task.number)
-                dup_task.ast = dup_task_ast
-                dup_task.aft = dup_task_aft
-                dup_task.processor_num = processor_num
-                self.dup_tasks.append(dup_task)
-                self.processors[dup_task.processor_num].time_line.append(
-                                Duration(-1, dup_task.ast, dup_task.aft))
-                self.processors[processor_num].time_line.sort(cmp=lambda x, y: cmp(x.start, y.start))
-                print('task %d dup on %s' % (pre_task.number, self.node_info[processor_num]))
 
     def reschedule(self):
-        """
-        After duplication, you should reschedule all tasks except redundant tasks.
-        """
         # clear the time line list
         for p in self.processors:
             p.time_line = filter(lambda duration: duration.task_num == -1, p.time_line)
@@ -303,6 +219,7 @@ class HEFT:
             task.aft = est + task.comp_cost[processor_num]
             self.processors[task.processor_num].time_line.append(Duration(task.number, task.ast, task.aft))
             self.processors[processor_num].time_line.sort(cmp=lambda x, y: cmp(x.start, y.start))
+
 
     def run(self):
         for task in self.tasks:
