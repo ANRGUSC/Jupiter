a1706a6d913687173aa8070fe3142f3ce1492982
Wasd5 <zhao115@usc.edu>
2020-04-18 11:05:10 -0700

    none

--- task_mapper/heft_mulhome/original/heft_dup.py
+++ task_mapper/heft_mulhome/original/heft_dup.py
@@ -315,15 +315,6 @@ class HEFT:
             print("link takeup time")
             for tl in link.time_line:
                 print("link task start_num, end_num, start_time, end_time" + str(tl.start_task_num) + str(tl.end_task_num) + str(start) + str(end))
-"""
-class LinkDuration
-    
-    def __init__(self, task1, task2, start, end):
-        self.start_task_num = task1
-        self.end_task_num = task2
-        self.start = start
-        self.end = end
-"""
         
 
     # output file is input_to_CIRCE
