fb491d23eacb6ee872c513358bbe7be5b295915c
Quynh <quynhngu@usc.edu>
2020-04-23 15:46:36 -0700

    debug 27

--- profilers/execution_profiler_mulhome/exec_docker_files_generator.py
+++ profilers/execution_profiler_mulhome/exec_docker_files_generator.py
@@ -59,10 +59,6 @@ COPY {app_file}/sample_input /centralized_scheduler/sample_input
 
 ADD {app_file}/configuration.txt /centralized_scheduler/DAG.txt
 
-# torch models
-RUN mkdir -p /root/.cache/torch/checkpoints/
-COPY {app_file}/scripts/modified_pytorch_source_code_files/models/resnet34-333f7ec4.pth /root/.cache/torch/checkpoints/resnet34-333f7ec4.pth
-
 
 ADD profilers/execution_profiler_mulhome/profiler_worker.py /centralized_scheduler/profiler.py
 
