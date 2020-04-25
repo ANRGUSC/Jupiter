b4d6214a3084921e87fa9003d619a0b44e5c6c6f
Jason A. Tran <jasonatran@gmail.com>
2020-04-24 17:35:32 -0700

    apac mergetb model

---
+++ k8s_bootstrap/mergetb/apac_model.py
@@ -0,0 +1,42 @@
+import mergexp as mx
+from mergexp.machine import image
+from mergexp.net import addressing, ipv4
+from mergexp.machine import cores, memory
+from mergexp.unit import gb
+
+# This mode is for creating a cluster with a single Ahi node (large instance)
+# 4/2020: FYI only Debian images work with Ahi nodes
+
+# set number of worker nodes
+NUM_WORKER_NODES = 4
+
+# minimum memory for all nodes, current DCOMP nodes include either 2GB or 8GB
+MIN_MEMORY = 4
+
+
+def ubuntu(name, version, min_memory=2):
+    dev = net.device(name, image == "ubuntu:" + version,
+                     memory >= gb(min_memory))
+    dev.props['group'] = version
+    return dev
+
+
+net = mx.Topology('k8s-bootstrap-example', addressing == ipv4)
+
+JUPITER_MASTER_NODE = ["master"]
+JUPITER_WORKER_NODES = ["n%d" % (x) for x in range(0, NUM_WORKER_NODES)]
+
+master = net.device(JUPITER_MASTER_NODE[0], memory >= gb(40))
+master.props["shape"] = "wye"
+master.props["color"] = "red"
+
+nodes = [ubuntu(name, '1804', MIN_MEMORY) for name in JUPITER_WORKER_NODES]
+
+# connect all worker nodes directly to master
+for n in nodes:
+    net.connect([master, n])
+
+# connect all worker nodes to LAN
+net.connect(nodes)
+
+mx.experiment(net)
