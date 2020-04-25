03d892ae3433696b7652f1a8946972152f0a264d
Quynh <quynhngu@usc.edu>
2020-04-25 12:11:55 -0700

    debug 72

--- circe/original/circe_docker_files_generator.py
+++ circe/original/circe_docker_files_generator.py
@@ -126,7 +126,7 @@ ENV NOTVISIBLE "in users profile"
 RUN echo "export VISIBLE=now" >> /etc/profile
 
 ADD circe/original/requirements.txt /requirements.txt
-# RUN pip3 install --upgrade pip
+RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
 
 RUN mkdir -p /centralized_scheduler/input
