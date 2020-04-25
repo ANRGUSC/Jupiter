2a4f7a7949dd2a65ba7b1861b166694e113b380f
Wasd5 <zhao115@usc.edu>
2020-04-24 22:03:57 -0700

    none

--- app_specific_files/sleep/scripts/task0.py
+++ app_specific_files/sleep/scripts/task0.py
@@ -2,7 +2,7 @@ import os
 import time
 import sys
 
-EXEC_TIME = 10
+EXEC_TIME = 1
 TASK_NAME = "task0"
 NUM_BLOCK = 8192
 
@@ -12,14 +12,7 @@ def task(input_files, pathin, pathout):
     filelist=[]
     filelist.append(input_files)
 
-    start_time = time.time()
-    count = 0
-    while(1):
-        count = count + 1
-        count = count - 1
-        cur_time = time.time()
-        if cur_time - start_time > EXEC_TIME:
-            break
+    time.sleep(EXEC_TIME)
     output_files = input_files.split('_')[0] + "_" + TASK_NAME
     cmd = "dd bs=1024 count=%d </dev/urandom >%s/%s" % (NUM_BLOCK, pathout, output_files)
     os.system(cmd)
