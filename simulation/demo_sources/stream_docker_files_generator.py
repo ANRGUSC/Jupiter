c092b25b25e56cd63cc6e46386605e4725f8fb8b
Quynh <quynhngu@usc.edu>
2020-04-29 16:58:18 -0700

    debug 83

--- simulation/demo_sources/stream_docker_files_generator.py
+++ simulation/demo_sources/stream_docker_files_generator.py
@@ -18,7 +18,7 @@ FROM ubuntu:16.04
 RUN apt-get -yqq update
 RUN apt-get -yqq install python3-pip python3-dev libssl-dev libffi-dev
 RUN apt-get install -y openssh-server mongodb
-ADD simulation/data_sources/requirements_data.txt /requirements.txt
+ADD simulation/demo_sources/requirements_data.txt /requirements.txt
 RUN apt-get -y install build-essential libssl-dev libffi-dev python3-dev
 RUN pip3 install --upgrade pip
 RUN apt-get install -y sshpass nano
@@ -44,7 +44,7 @@ ADD jupiter_config.ini /jupiter_config.ini
 RUN mkdir generated_stream 
 ADD simulation/demo_sources/start_home.sh /start.sh
 ADD simulation/demo_sources/ds_stream.py /ds_stream.py
-ADD simulation/data /data
+ADD simulation/demo_sources/data /data
 
 RUN chmod +x /start.sh
 
