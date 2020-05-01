d4614259df787b386b8666c6a631cf985ba7ffcb
Jason A. Tran <jasonatran@gmail.com>
2020-05-01 13:21:23 -0700

    build_push_exec.py: code styling

--- mulhome_scripts/build_push_exec.py
+++ mulhome_scripts/build_push_exec.py
@@ -1,8 +1,9 @@
-__author__ = "Pradipta Ghosh, Pranav Sakulkar, Quynh Nguyen, Jason A Tran,  Bhaskar Krishnamachari"
-__copyright__ = "Copyright (c) 2019, Autonomous Networks Research Group. All rights reserved."
+__author__ = """Pradipta Ghosh, Pranav Sakulkar, Quynh Nguyen, Jason A Tran,
+                Bhaskar Krishnamachari"""
+__copyright__ = """Copyright (c) 2019, Autonomous Networks Research Group. All
+                   rights reserved."""
 __license__ = "GPL"
 __version__ = "2.1"
-
 import sys
 sys.path.append("../")
 import os
@@ -10,69 +11,81 @@ import configparser
 import jupiter_config
 import logging
 
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def prepare_global_info():
     """Read configuration information from ``app_config.ini``
-    
+
     Returns:
-        - list: port_list_home - The list of ports to be exposed in the exec home dockers
-        - list: port_list_worker - The list of ports to be exposed in the exec worker dockers
+        - list: port_list_home - The list of ports to be exposed in the exec
+                                    home dockers
+        - list: port_list_worker - The list of ports to be exposed in the exec
+                                    worker dockers
     """
     jupiter_config.set_globals()
 
-    INI_PATH  = jupiter_config.APP_PATH + 'app_config.ini'
+    INI_PATH = jupiter_config.APP_PATH + 'app_config.ini'
     config = configparser.ConfigParser()
     config.read(INI_PATH)
     logging.debug(INI_PATH)
     logging.debug(config)
     logging.debug(config["DOCKER_PORT"])
     sys.path.append(jupiter_config.EXEC_PROFILER_PATH)
-    
+
     port_list_home = []
     port_list_home.append(jupiter_config.SSH_DOCKER)
     port_list_home.append(jupiter_config.MONGO_DOCKER)
     port_list_home.append(jupiter_config.FLASK_DOCKER)
     for key in config["DOCKER_PORT"]:
-      logging.debug(config["DOCKER_PORT"][key])
-      port_list_home.append(config["DOCKER_PORT"][key])
-    logging.debug('The list of ports to be exposed in the exec home dockers are %s', " ".join(port_list_home))
+        logging.debug(config["DOCKER_PORT"][key])
+        port_list_home.append(config["DOCKER_PORT"][key])
+    logging.debug('The list of ports to be exposed in the exec home dockers',
+                  'are {}'.format(" ".join(port_list_home)))
 
     port_list_worker = []
     port_list_worker.append(jupiter_config.SSH_DOCKER)
     port_list_worker.append(jupiter_config.MONGO_DOCKER)
     port_list_worker.append(jupiter_config.FLASK_DOCKER)
-    logging.debug('The list of ports to be exposed in the exec worker dockers are %s', " ".join(port_list_worker))
+    logging.debug('The list of ports to be exposed in the exec worker dockers',
+                  'are {}'.format(" ".join(port_list_worker)))
 
     return port_list_home, port_list_worker
 
+
 def build_push_exec():
-    """Build execution profiler home and worker image from Docker files and push them to the Dockerhub.
+    """Build execution profiler home and worker image from Docker files and
+       push them to the Dockerhub.
     """
     port_list_home, port_list_worker = prepare_global_info()
     import exec_docker_files_generator as dc
 
+    os.chdir(jupiter_config.EXEC_PROFILER_PATH)
 
-    os.chdir(jupiter_config.EXEC_PROFILER_PATH )
+    home_file = dc.write_exec_home_docker(
+        username=jupiter_config.USERNAME,
+        password=jupiter_config.PASSWORD,
+        app_file=jupiter_config.APP_NAME,
+        ports=" ".join(port_list_home)
+    )
 
-    home_file = dc.write_exec_home_docker(username = jupiter_config.USERNAME,
-                     password = jupiter_config.PASSWORD,
-                     app_file = jupiter_config.APP_NAME,
-                     ports = " ".join(port_list_home))
+    worker_file = dc.write_exec_worker_docker(
+        username=jupiter_config.USERNAME,
+        password=jupiter_config.PASSWORD,
+        app_file=jupiter_config.APP_NAME,
+        ports=" ".join(port_list_worker)
+    )
 
-    worker_file = dc.write_exec_worker_docker(username = jupiter_config.USERNAME,
-                     password = jupiter_config.PASSWORD,
-                     app_file=jupiter_config.APP_NAME,
-                     ports = " ".join(port_list_worker))
-
-    cmd = "sudo docker build -f %s ../.. -t %s"%(home_file,jupiter_config.EXEC_HOME_IMAGE)
+    cmd = "sudo docker build -f {} ../.. -t {}".format(
+        home_file, jupiter_config.EXEC_HOME_IMAGE)
     os.system(cmd)
     os.system("sudo docker push " + jupiter_config.EXEC_HOME_IMAGE)
 
-    cmd = "sudo docker build -f %s ../.. -t %s"%(worker_file,jupiter_config.EXEC_WORKER_IMAGE)
+    cmd = "sudo docker build -f {} ../.. -t {}".format(
+        worker_file, jupiter_config.EXEC_WORKER_IMAGE)
     os.system(cmd)
     os.system("sudo docker push " + jupiter_config.EXEC_WORKER_IMAGE)
+
 
 if __name__ == '__main__':
     build_push_exec()
