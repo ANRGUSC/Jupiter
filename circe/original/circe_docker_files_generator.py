54f8f67de9718d4921cd50900081e3c625fc177c
Quynh <quynhngu@usc.edu>
2020-05-01 17:23:33 -0700

    debug 110

--- circe/original/circe_docker_files_generator.py
+++ circe/original/circe_docker_files_generator.py
@@ -83,8 +83,9 @@ template_worker ="""\
 # Instructions copied from - https://hub.docker.com/_/python/
 FROM ubuntu:18.04
 
-RUN apt-get -yqq update && apt-get install -y --no-install-recommends apt-utils
+RUN apt-get -yqq update
 RUN apt-get -yqq install python3-pip python3-dev libssl-dev libffi-dev
+
 RUN apt-get install -yqq openssh-client openssh-server bzip2 wget net-tools sshpass screen --fix-missing
 RUN apt-get install -y vim
 RUN apt-get install g++ make openmpi-bin libopenmpi-dev -y
