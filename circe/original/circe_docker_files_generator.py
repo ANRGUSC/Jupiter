b29e2f53e0ccb6ddb80d8b8ec7d108a652147913
Quynh <quynhngu@usc.edu>
2020-05-02 11:11:43 -0700

    debug 120

--- circe/original/circe_docker_files_generator.py
+++ circe/original/circe_docker_files_generator.py
@@ -29,12 +29,14 @@ RUN pip3 install -r requirements.txt
 
 RUN echo '{username}:{password}' | chpasswd
 RUN sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+RUN sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+RUN sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 # SSH login fix. Otherwise user is kicked off after login
 RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 ENV NOTVISIBLE "in users profile"
-RUN echo "export VISIBLE=now" >> /etc/profile
+RUN echo "export VISIBLE=now" >> /etc/profile 
 
 # Create the mongodb directories
 RUN mkdir -p /mongodb/data
