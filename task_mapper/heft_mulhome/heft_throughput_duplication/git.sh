9cd7d7a1c3e481e61f5c90490345be730804835a
Wasd5 <zhao115@usc.edu>
2020-05-01 21:42:11 -0700

    none

--- task_mapper/heft_mulhome/heft_throughput_duplication/git.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "none"
-git push
---
+++ task_mapper/heft_mulhome/original/utilities.py
@@ -0,0 +1,45 @@
+def get_bottleneck(links, processors):
+    """
+    given current processor and link usage, return the id of resource with max takeup time 
+    (without considering pipelined wait times)
+    input: an array of links and an array of processors (with takeup time)
+    output: id of bottleneck resource (string)
+    """
+    max_time = 0.0
+    bottleneck = ""
+    for link in links:
+        if len(link.time_line) != 0:
+            if link.time_line[-1].end > max_time:
+                max_time = link.time_line[-1].end
+                bottleneck = link.id
+                
+    for processor in processors:
+        if len(processor.time_line) != 0:
+            if processor.time_line[-1].end > max_time:
+                max_time = processor.time_line[-1].end
+                bottleneck = str(processor.id)
+                
+    return bottleneck
+    
+    
+def is_link(rsrc_id):
+    """
+    return true if the bottleneck id represents a link
+    false if processor
+    """
+    for c in rsrc_id:
+        if c == '_':
+            return True
+    return False
+            
+
+def get_idle_proc_id(processors):
+    """
+    given all the current state of processors, return idle ones (hash set)
+    """
+    idle_proc_ids = set()
+    for processor in processors:
+        if len(processor.time_line) == 0:
+            idle_proc_ids.add(processor.number)
+    return idle_proc_ids
+
