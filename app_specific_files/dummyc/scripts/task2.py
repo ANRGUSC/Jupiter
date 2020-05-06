4ccdd8a966fd9c3453088a521ef2ea6c9408a6da
Quynh <quynhngu@usc.edu>
2020-05-05 20:25:52 -0700

    testing democ 5

--- app_specific_files/dummyc/scripts/task2.py
+++ app_specific_files/dummyc/scripts/task2.py
@@ -5,14 +5,17 @@ import math
 import random
 import subprocess
 
-def task():
+def task(filename,pathin,pathout):
      filename= "task2.c"
      subprocess.call(["gcc",filename])
      subprocess.call("./a.out")
         
 
 def main():
-    task()
+	filelist = '1botnet.ipsum'
+	outpath = os.path.join(os.path.dirname(__file__), 'sample_input/')
+	outfile = task(filelist, outpath, outpath)
+	return outfile
     
 if __name__ == "__main__":
     main()
