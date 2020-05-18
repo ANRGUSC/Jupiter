a909fc1dd9d162593ff697942f32b734bf8f3385
Wasd5 <zhao115@usc.edu>
2020-05-18 15:17:08 -0700

    none

--- mulhome_scripts/k8s_circe_scheduler.py
+++ mulhome_scripts/k8s_circe_scheduler.py
@@ -383,7 +383,7 @@ def k8s_circe_scheduler(dag_info, temp_info, app_name):
         
             pod_name = app_name+"-"+task
             #Generate the yaml description of the required deployment for each task
-            node_name_here = hosts[task.split('-')[0]][task.split('-')[-1]*2-1]
+            node_name_here = hosts[task.split('-')[0]][int(task.split('-')[-1])*2-1]
             dep = write_circe_deployment_specs(name = pod_name, node_name = node_name_here,
                 image = jupiter_config.WORKER_IMAGE, child = nexthosts, task_name=task,
                 child_ips = next_svc, host = hosts.get(task)[1], dir = '{}',
