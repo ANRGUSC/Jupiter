e83636b9e63ecd3df84939b436d88e6f76d6068d
Wasd5 <zhao115@usc.edu>
2020-05-19 19:52:39 -0700

    none

--- mulhome_scripts/delete_all_circe.py
+++ mulhome_scripts/delete_all_circe.py
@@ -152,7 +152,7 @@ def delete_all_circe(app_name):
 
     # Check if there is a replicaset running by using the label app=home
     # The label of kubernets are used to identify replicaset associate to each task
-    label = "app="+app_name+"-home"
+    label = "app="+app_name+"-home-1"
     # label = "app=home"
     resp = extensions_v1_beta1_api.list_namespaced_replica_set(label_selector = label,namespace = namespace)
     # if a replicaset exist, delete it
