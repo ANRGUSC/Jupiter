94ba83711928190dbfef35797e34d6e9c928c96f
Wasd5 <zhao115@usc.edu>
2020-05-20 14:57:33 -0700

    none

--- circe/original/scheduler.py
+++ circe/original/scheduler.py
@@ -221,9 +221,9 @@ def transfer_data_scp(ID,user,pword,source, destination):
     ts = -1
     print("############################# DEBUG #############################")
     print(ID, user, pword, source, destination)
+    print(combined_map)
     while retry < num_retries:
         try:
-            print(combined_map)
             nodeIP = combined_ip_map[ID]
             print(nodeIP)
             cmd = "sshpass -p %s scp -P %s -o StrictHostKeyChecking=no -r %s %s@%s:%s" % (pword, ssh_port, source, user, nodeIP, destination)
