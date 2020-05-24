96b26f126e8d83974e50676ec4fdd40cbc87481d
Wasd5 <zhao115@usc.edu>
2020-05-23 23:47:17 -0700

    none

--- task_mapper/heft_mulhome/original/heft_dup.py
+++ task_mapper/heft_mulhome/original/heft_dup.py
@@ -307,7 +307,6 @@ class HEFT:
                 new_node = self.processors[new_node_id]
                 dup.duplicate(self.links, self.processors, self.tasks, self.comp_cost, self.data, self.quaratic_profile, 
                 btnk_id, new_node, min_btnk, task_ids_to_dup, task_ids_to_recv, parent_tasks, self.task_names, files_to_dst, files_from_src)
-                self.display_result(1)
             else:
                 break
                 #spt = split.Split()
