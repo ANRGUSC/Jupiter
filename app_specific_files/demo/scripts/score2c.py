6dc67351954e1d26dbe4224ab9de35af309fd609
Chien-Sheng Yang <39869317+paul555111@users.noreply.github.com>
2020-06-02 23:14:20 -0700

    Update score2c.py

--- app_specific_files/demo/scripts/score2c.py
+++ app_specific_files/demo/scripts/score2c.py
@@ -70,7 +70,7 @@ def task(filelist, pathin, pathout):
     return outlist
     
 def main():
-    filelist= ['lccenc2_score2c_job2_resnet0_storeclass2_master_resnet0_n04146614_1_jobid_0.csv']
+    filelist= ['lccenc2_score2c_job2.csv']
     outpath = os.path.join(os.path.dirname(__file__), 'sample_input/')
     outfile = task(filelist, outpath, outpath)
     return outfile
