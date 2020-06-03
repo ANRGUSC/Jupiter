9fb9af5f2232a5cc092efadbaabe826914692dc4
Chien-Sheng Yang <39869317+paul555111@users.noreply.github.com>
2020-06-02 23:05:25 -0700

    Update score1a.py

--- app_specific_files/demo/scripts/score1a.py
+++ app_specific_files/demo/scripts/score1a.py
@@ -74,7 +74,7 @@ def task(filelist, pathin, pathout):
 
 
 def main():
-    filelist= ['lccenc1_score1a_job2_resnet0_storeclass1_master_resnet0_n03345487_10_jobid_0.csv']
+    filelist= ['lccenc1_score1a_job2.csv']
     outpath = os.path.join(os.path.dirname(__file__), 'sample_input/')
     outfile = task(filelist, outpath, outpath)
     return outfile
