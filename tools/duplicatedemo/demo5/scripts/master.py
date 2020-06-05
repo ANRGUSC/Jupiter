402320a97431a3119decdaff8f7d63dbfe13ca28
caravansary83 <quynhngu@usc.edu>
2020-06-04 20:27:44 -0700

    checking function part 2

--- tools/duplicatedemo/demo5/scripts/master.py
+++ tools/duplicatedemo/demo5/scripts/master.py
@@ -185,7 +185,7 @@ def get_and_send_missing_images(pathin):
         logging.debug(image_file)
         file_name_wo_jobid = image_file.split("_jobid_")[0]
         source_path = os.path.join(pathin, file_name_wo_jobid + ".JPEG")
-        file_name = 'master_' + image_file
+        file_name = 'master_master_master_master_' + image_file
         logging.debug('Transfer the file')
         destination_path = os.path.join('/centralized_scheduler/input',file_name)
         logging.debug(destination_path)
