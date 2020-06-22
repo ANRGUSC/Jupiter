6a75bac49138c2ce365ccc808c23dd02f89e59bf
Jason A. Tran <jasonatran@gmail.com>
2020-06-21 20:42:33 -0700

    plot fixes, filtered logs added

--- tools/ccdag/plot_log_data.py
+++ tools/ccdag/plot_log_data.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import matplotlib.pyplot as plt
+from statistics import mean
 
 # SEE README.md for more information on naming conventions!
 
@@ -9,7 +10,7 @@ import matplotlib.pyplot as plt
 # Letters indicate test number (to differentiate from the flags)
 # postfix "sleep" indicates if artificial sleeps are injected in the test
 # examples: "11a", "01a", "01b" "01a-sleep"
-TEST_INDICATORS = "01a-sleep"
+TEST_INDICATORS = "11c-"
 COMM_TIMES = "filtered_logs/{}comm.log".format(TEST_INDICATORS)
 MAKESPAN = "filtered_logs/{}makespan.log".format(TEST_INDICATORS)
 TASK_TIMES = "filtered_logs/{}task.log".format(TEST_INDICATORS)
@@ -25,8 +26,8 @@ def plot_makespan(makespan_log, file_prefix):
     fig1 = plt.figure()
     plt.plot(makespans, '.')
     plt.title("makespan vs image")
-
-    fig1.savefig('figures/makespans{}.png'.format(file_prefix))
+    plt.ylabel("seconds")
+    fig1.savefig('figures/{}makespans.png'.format(file_prefix))
 
 
 def plot_comm_times(comm_log, file_prefix):
@@ -38,45 +39,35 @@ def plot_comm_times(comm_log, file_prefix):
     score_to_preagg = []
     preagg_to_lccdec = []
 
+    source_to_dest = ['master_to_resnet', 'resnet_to_storeclass',
+    'storeclass_to_lccenc', 'lccenc_to_score', 'score_to_preagg',
+    'preagg_to_lccdec']
+
     for k, v in comm_log.items():
-        if k[1].startswith('master') and k[2].startswith('resnet'):
+        if k[1].startswith('master') and k[0].startswith('resnet'):
             master_to_resnet.append(v)
-        if k[1].startswith('resnet') and k[2].startswith('storeclass'):
+        if k[1].startswith('resnet') and k[0].startswith('storeclass'):
             resnet_to_storeclass.append(v)
-        if k[1].startswith('storeclass') and k[2].startswith('lccenc'):
+        if k[1].startswith('storeclass') and k[0].startswith('lccenc'):
             storeclass_to_lccenc.append(v)
-        if k[1].startswith('lccenc') and k[2].startswith('score'):
+        if k[1].startswith('lccenc') and k[0].startswith('score'):
             lccenc_to_score.append(v)
-        if k[1].startswith('score') and k[2].startswith('preagg'):
+        if k[1].startswith('score') and k[0].startswith('preagg'):
             score_to_preagg.append(v)
-        if k[1].startswith('preagg') and k[2].startswith('lccdec'):
+        if k[1].startswith('preagg') and k[0].startswith('lccdec'):
             preagg_to_lccdec.append(v)
 
-    fig1 = plt.figure()
-    plt.plot(master_to_resnet, '.')
-    plt.title("master_to_resnet communication time vs job")
-    fig2 = plt.figure()
-    plt.plot(resnet_to_storeclass, '.')
-    plt.title("resnet_to_storeclass communication time vs job")
-    fig3 = plt.figure()
-    plt.plot(storeclass_to_lccenc, '.')
-    plt.title("storeclass_to_lccenc communication time vs job")
-    fig4 = plt.figure()
-    plt.plot(lccenc_to_score, '.')
-    plt.title("lccenc_to_score communication time vs job")
-    fig5 = plt.figure()
-    plt.plot(score_to_preagg, '.')
-    plt.title("score_to_preagg communication time vs job")
-    fig6 = plt.figure()
-    plt.plot(preagg_to_lccdec, '.')
-    plt.title("preagg_to_lccdec communication time vs job")
-
-    fig1.savefig('figures/{}_master_to_resnet_comm_times.png'.format(file_prefix))
-    fig2.savefig('figures/{}_resnet_to_storeclass_comm_times.png'.format(file_prefix))
-    fig3.savefig('figures/{}_storeclass_to_lccenc_comm_times.png'.format(file_prefix))
-    fig4.savefig('figures/{}_lccenc_to_score_comm_times.png'.format(file_prefix))
-    fig5.savefig('figures/{}_score_to_preagg_comm_times.png'.format(file_prefix))
-    fig6.savefig('figures/{}_preagg_to_lccdec_comm_times.png'.format(file_prefix))
+    for src_dst in source_to_dest:
+        fig = plt.figure()
+        plt.plot(eval(src_dst), '.')
+        average = mean(eval(src_dst))
+        print("Comm: average of {} is {} seconds".format(src_dst, average))
+        plt.title("{} {} communication time vs job\naverage = {}s"
+                  .format(file_prefix, src_dst, average))
+        plt.ylabel("seconds")
+        plt.tight_layout()
+        fig.savefig('figures/{}_{}_comm_times.png'
+                    .format(file_prefix, src_dst))
 
 
 def plot_task_timings(task_log, file_prefix):
@@ -93,6 +84,15 @@ def plot_task_timings(task_log, file_prefix):
     resnet_wait_times = []
     score_svc_times = []
     score_wait_times = []
+
+    task_and_statistic = [
+        ['lccdec_svc_times', 'lccdec_wait_times'],
+        ['lccenc_svc_times', 'lccenc_wait_times'],
+        ['master_svc_times', 'master_wait_times'], 
+        ['preagg_svc_times', 'preagg_wait_times'],
+        ['resnet_svc_times', 'resnet_wait_times'],
+        ['score_svc_times', 'score_wait_times']
+    ]  
 
     # keys are tuples:
     # ('task_name','local_input_file')
@@ -122,44 +122,22 @@ def plot_task_timings(task_log, file_prefix):
             score_svc_times.append(float(v[6]))
             score_wait_times.append(float(v[7]))
 
-    fig1 = plt.figure()
-    plt.plot(lccdec_svc_times, '.')
-    plt.plot(lccdec_wait_times, 'y+')
-    plt.title("lccdec service time (.) and wait times (+) vs. job instance")
-
-    fig2 = plt.figure()
-    plt.plot(lccenc_svc_times, '.')
-    plt.plot(lccenc_wait_times, 'y+')
-    plt.title("lccenc service time (.) and wait times (+) vs. job instance")
-
-    fig3 = plt.figure()
-    plt.plot(master_svc_times, '.')
-    plt.plot(master_wait_times, 'y+')
-    plt.title("master service time (.) and wait times (+) vs. job instance")
-
-    fig4 = plt.figure()
-    plt.plot(preagg_svc_times, '.')
-    plt.plot(preagg_wait_times, 'y+')
-    plt.title("preagg service time (.) and wait times (+) vs. job instance")
-
-    fig5 = plt.figure()
-    plt.plot(resnet_svc_times, '.')
-    plt.plot(resnet_wait_times, 'y+')
-    plt.title("resnet service time (.) and wait times (+) vs. job instance")
-
-    fig6 = plt.figure()
-    plt.plot(score_svc_times, '.')
-    plt.plot(score_wait_times, 'y+')
-    plt.title("score service time (.) and wait times (+) vs. job instance")
-
-    # uncomment to save to files
-    fig1.savefig('figures/{}_lccdec_svc_times.png'.format(file_prefix))
-    fig2.savefig('figures/{}_lccenc_svc_times.png'.format(file_prefix))
-    fig3.savefig('figures/{}_master_svc_times.png'.format(file_prefix))
-    fig4.savefig('figures/{}_preagg_svc_times.png'.format(file_prefix))
-    fig5.savefig('figures/{}_resnet_svc_times.png'.format(file_prefix))
-    fig6.savefig('figures/{}_score_svc_times.png'.format(file_prefix))
-
+    for task in task_and_statistic:
+        fig = plt.figure()
+        plt.plot(eval(task[0]), '.')
+        plt.plot(eval(task[1]), 'y+')
+        svc_time_avg = mean(eval(task[0]))
+        print("{} average is {}s".format(task[0], svc_time_avg))
+        wait_time_avg = mean(eval(task[1]))
+        print("{} average is {}s".format(task[1], wait_time_avg))
+        plt.title(
+            "{} {} service time (.) and wait times (+) vs. job instance".format(file_prefix, task[0][0:6]) +
+            "\nservice time avg = {}".format(svc_time_avg) +
+            "\nwait time avg = {}".format(wait_time_avg)
+        )
+        plt.ylabel("seconds")
+        plt.tight_layout()
+        fig.savefig('figures/{}_{}_svc_times.png'.format(file_prefix, task[0][0:6]))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -167,14 +145,25 @@ if __name__ == '__main__':
 
     os.makedirs('figures', exist_ok=True)
 
-    with open(TASK_TIMES, 'r') as f:
-        task_log = eval(f.read())
-        plot_task_timings(task_log, TEST_INDICATORS)
+    print("Graphing and calculating averages for test {}".format(TEST_INDICATORS))
 
-    with open(COMM_TIMES, 'r') as f:
-        comm_log = eval(f.read())
-        plot_comm_times(comm_log, TEST_INDICATORS)
+    try:
+        with open(TASK_TIMES, 'r') as f:
+            task_log = eval(f.read())
+            plot_task_timings(task_log, TEST_INDICATORS)
+    except FileNotFoundError:
+        print("{} does not exist".format(TASK_TIMES))
 
-    with open(MAKESPAN, 'r') as f:
-        makespan_log = eval(f.read())
-        plot_makespan(makespan_log, TEST_INDICATORS)
+    try:
+        with open(COMM_TIMES, 'r') as f:
+            comm_log = eval(f.read())
+            plot_comm_times(comm_log, TEST_INDICATORS)
+    except FileNotFoundError:
+        print("{} does not exist".format(TASK_TIMES))
+
+    try:
+        with open(MAKESPAN, 'r') as f:
+            makespan_log = eval(f.read())
+            plot_makespan(makespan_log, TEST_INDICATORS)
+    except FileNotFoundError:
+        print("{} does not exist".format(TASK_TIMES))
