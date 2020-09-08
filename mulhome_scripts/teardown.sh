db4cfd307e0820f27a65e4b2be447add40472335
Wasd5 <zhao115@usc.edu>
2020-09-08 01:09:46 -0700

    none

--- mulhome_scripts/teardown.sh
+++ mulhome_scripts/teardown.sh
@@ -1,4 +1,4 @@
-kubectl delete svc -n zxc-circe
+kubectl delete svc --all -n zxc-circe
 kubectl delete deployment --all -n zxc-circe
 kubectl delete pods --all -n zxc-circe
 kubectl delete replicaset --all -n zxc-circe
