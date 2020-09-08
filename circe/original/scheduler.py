ded3d2b1ba243f39ca5141dc9ace5d8d28b782a5
Wasd5 <zhao115@usc.edu>
2020-09-07 19:47:59 -0700

    none

--- circe/original/scheduler.py
+++ circe/original/scheduler.py
@@ -277,8 +277,7 @@ class MyHandler(pyinotify.ProcessEvent):
         Args:
             event (ProcessEvent): a new file is created
         """
-
-        print("Received file as output - %s." % event.pathname)  
+        logging.debug("Received file as output - %s." % event.pathname)  
 
         if RUNTIME == 1:   
             ts = time.time() 
@@ -365,7 +364,7 @@ class Handler(pyinotify.ProcessEvent):
         probs = []
         for var in tmp:
             tasks.append(var.split('/')[0])
-            probs.append(1 if len(var.split('/')) == 1 else var.split('/')[1])
+            probs.append(var.split('/')[1])
         if len(probs) == 1:
             return tasks[0]
         chosen_task = ""
