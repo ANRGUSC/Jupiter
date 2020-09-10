e5f6b750523b8aaa7db69763eab8f45ff3a7a99e
Wasd5 <zhao115@usc.edu>
2020-09-09 20:01:25 -0700

    none

--- circe/original/monitor.py
+++ circe/original/monitor.py
@@ -436,7 +436,7 @@ def main():
     global taskmap, taskname, taskmodule, filenames,files_out, node_name, home_node_host_port, all_nodes, all_nodes_ips
 
     configs = json.load(open('/centralized_scheduler/config.json'))
-    taskmap = configs["taskname_map"][sys.argv[len(sys.argv)-1]]
+    taskmap = configs["taskname_map"][(sys.argv[len(sys.argv)-1).split('-')]]
     taskname = taskmap[0]
     if taskmap[1] == True:
         taskmodule = __import__(taskname)
