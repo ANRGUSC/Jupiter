a9ce182e4cd2e2eb433538c3b578bb0c310ab3d5
Wasd5 <zhao115@usc.edu>
2020-10-04 00:20:55 -0700

    none

--- task_mapper/heft_mulhome/original/heft_dup.py
+++ task_mapper/heft_mulhome/original/heft_dup.py
@@ -354,7 +354,9 @@ class HEFT:
     
     
     def run_dup_split(self):
-        while True:
+        cur = 0
+        while True and cur <= 6:
+            cur += 1
             btnk_id = self.get_btnk_id()
             spt = split.Split()
             if self.is_link(btnk_id):
