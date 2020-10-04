195665215d1a0fe62eb77e37fec756514114e9fb
Wasd5 <zhao115@usc.edu>
2020-10-04 12:35:31 -0700

    none

--- app_specific_files/count/scripts/task12.py
+++ app_specific_files/count/scripts/task12.py
@@ -2,9 +2,9 @@ import os
 import time
 import sys
 
-LOOP_RANGE = 6000000
-TASK_NAME = "task9"
-NUM_BLOCK = 1000
+LOOP_RANGE = 7000000
+TASK_NAME = "task12"
+NUM_BLOCK = 2000
 
 def task(input_files, pathin, pathout):
 
