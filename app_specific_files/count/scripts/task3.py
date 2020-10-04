6e1bc09f34083f2a2a58cf77ea9bb3763b049f12
Wasd5 <zhao115@usc.edu>
2020-10-03 20:53:07 -0700

    none

--- app_specific_files/count/scripts/task3.py
+++ app_specific_files/count/scripts/task3.py
@@ -8,19 +8,20 @@ NUM_BLOCK = 3000
 
 def task(input_files, pathin, pathout):
 
+
     filelist=[]
-    filelist.append(input_files)  
+    filelist.append(input_files)
+
     count = 0
     for i in range(LOOP_RANGE):
         count = count + 1
         count = count - 1
-    # output_files = input_files[0].split('_')[0] + "_" + TASK_NAME
 
     output_files = input_files[0].split('_')[0] + "_" + TASK_NAME
+    # output_files = input_files.split('_')[0] + "_" + TASK_NAME
     cmd = "dd bs=1024 count=%d </dev/urandom >%s/%s" % (NUM_BLOCK, pathout, output_files)
     os.system(cmd)
     return [os.path.join(pathout, output_files)]
-    return []
 
 def main():
     filelist= 'input0'
