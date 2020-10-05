d5f3958dc66c951091cafd7ad37b6a9595f0ba6d
Wasd5 <zhao115@usc.edu>
2020-10-04 20:55:39 -0700

    none

--- app_specific_files/imgp/scripts/dummy.py
+++ app_specific_files/imgp/scripts/dummy.py
@@ -14,21 +14,22 @@ import cv2 as cv
 
 def task(input_files, pathin, pathout):
 
-    src = cv.imread(os.path.join(pathin, input_files))
-    cv.imwrite(os.path.join(pathout, input_files), src)
-    return [os.path.join(pathout, input_files)]
+    for onefile in input_files:
+        src = cv.imread(os.path.join(pathin, onefile))
+        cv.imwrite(os.path.join(pathout, onefile), src)
+    return [os.path.join(pathout, input_files[0]), os.path.join(pathout, input_files[1])]
 
 
 
 def main():
-    filelist= 'test_left.jpeg'
+    filelist= ['test_left.jpeg', 'test_right.jpeg']
     outpath = os.path.join(os.path.dirname(__file__), "sample_input/")
     outfile = task(filelist, outpath, outpath)
     return outfile
 
 if __name__ == '__main__':
 
-    filelist= 'test_left.jpeg'
+    filelist= ['test_left.jpeg', 'test_right.jpeg']
     task(filelist, '/home/zxc/Desktop/imgptest/sample_input', '/home/zxc/Desktop/imgptest/generated_files')
 
 
