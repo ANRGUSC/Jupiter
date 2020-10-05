1eb74d4d5343e4686913d4037c838afa169d5431
Wasd5 <zhao115@usc.edu>
2020-10-04 18:00:32 -0700

    none

--- app_specific_files/imgp/scripts/stitch.py
+++ app_specific_files/imgp/scripts/stitch.py
@@ -144,7 +144,7 @@ def task(filelist, pathin, pathout):
 
 
 def main():
-    filelist= ['test_left_preprocess1.jpeg', 'test_left_preprocess2.jpeg']
+    filelist= ['test_left_preprocess1.jpeg', 'test_right_preprocess2.jpeg']
     outpath = os.path.join(os.path.dirname(__file__), "sample_input/")
     outfile = task(filelist, outpath, outpath)
     return outfile
