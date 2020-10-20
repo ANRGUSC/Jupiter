bd8a0f2801abc927749769a5b991ebfead9d9ca7
caravansary83 <quynhngu@usc.edu>
2020-10-20 16:41:09 -0700

    testing on dcomp

--- k8s_bootstrap/mergetb/apac_model.py
+++ k8s_bootstrap/mergetb/apac_model.py
@@ -5,10 +5,10 @@ from mergexp.machine import cores, memory
 from mergexp.unit import gb
 
 # set number of minnow worker nodes
-NUM_MINNOWS = 50
+NUM_MINNOWS = 0
 
 # set number of rohu worker nodes
-NUM_ROHUS = 50
+NUM_ROHUS = 8
 
 
 def ubuntu(name, version, min_memory=2):
