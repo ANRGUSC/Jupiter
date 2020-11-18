13d8dd6bd39503094ee3f7b981ff1b0b7dd4fbce
caravansary83 <quynhngu@usc.edu>
2020-11-18 00:20:42 -0800

    change ccdag

--- app_specific_files/demo/ccdag.py
+++ app_specific_files/demo/ccdag.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
-#JUPITER_CONFIG_INI_PATH = '/jupiter/build/jupiter_config.ini' #docker
-JUPITER_CONFIG_INI_PATH = '../../jupiter_config.ini' # local
+JUPITER_CONFIG_INI_PATH = '/jupiter/build/jupiter_config.ini' #docker
+#JUPITER_CONFIG_INI_PATH = '../../jupiter_config.ini' # local
 
-EXP_ID = 'c'
+EXP_ID = 'na'
 
 CODING_PART1 = 0
-RESNETS_THRESHOLD = 3 #1 or 3
+RESNETS_THRESHOLD = 1 #1 or 3
 
 CODING_PART2 = 1
 
@@ -19,7 +19,7 @@ RESNET_POLL_INTERVAL = 1
 MASTER_TO_RESNET_TIME = 10
 
 STREAM_INTERVAL = 60
-NUM_IMAGES = 100
-NUM_CLASS =11
-NUM_DATASOURCES = 100
+NUM_IMAGES = 50
+NUM_CLASS =19
+NUM_DATASOURCES = 360
 classlist = ['fireengine', 'schoolbus', 'whitewolf',  'tiger', 'kitfox', 'persiancat', 'leopard',   'americanblackbear', 'mongoose', 'zebra',  'hippopotamus',  'waterbuffalo', 'ram', 'impala', 'arabiancamel', 'otter','ox','lion','hog','hyena']
